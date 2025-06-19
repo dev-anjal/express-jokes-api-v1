8e68a131543010c204f1d4efb3e919075ed32045
FlameAnjal <kinganjalap@gmail.com>
2025-06-19 13:53:37 +0545

    ft. two part Joke

--- Jokes/jokes.js
+++ Jokes/jokes.js
@@ -1,16 +1,32 @@
-const jokes = require('./jokes.json')
+const jokes = require('./jokes.json');
+const twoPartJokes = require('./jokes_with_uid_and_type.json');
 
 function getJokes(){
     return jokes;
 }
 
+function getRandomNumber(x){
+    let randomNum= Math.floor(Math.random()*x)
+    return randomNum;
+}
+
+
 function getOneJoke(){
-    const randomNum= Math.floor(Math.random()* jokes.length);
+    var randomNum= getRandomNumber(50);
     const joke =  jokes[randomNum];
     return joke;
 }
 
+function getTwoPartJoke(cat=null){
+    if(cat==null){
+        const randomNum = getRandomNumber(100);
+        const joke = twoPartJokes[randomNum];
+        return joke;
+    }
+}
+
 module.exports={
     getJokes,
-    getOneJoke
+    getOneJoke,
+    getTwoPartJoke
 };
--- Routes/jokes.js
+++ Routes/jokes.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const {getJokes, getOneJoke}= require('../Jokes/jokes')
+const {getJokes, getOneJoke , getTwoPartJoke}= require('../Jokes/jokes')
 
 router.get('/get-one-liner',(req,res)=>{
 res.json(getOneJoke())
+})
+
+router.get('/get-two-part', (req,res)=>{
+    res.json(getTwoPartJoke())
 })
 
 module.exports=router;
